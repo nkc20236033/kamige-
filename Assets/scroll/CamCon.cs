94110e2172c15adadd553102e1ab99e572e8cc6c
nkc20236054 <nkc23.kuzuha.mitsuki@gmail.com>
2024-04-12 11:29:40 +0900

    背景関連、メインカメラをプレイヤーの子オブジェクトにするのを忘れずに

---
+++ Assets/scroll/CamCon.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamCon : MonoBehaviour
+{
+    //í«â¡Å@XÇ∆YÇÃè„å¿
+    float yLimit = 0f;
+
+    void Update()
+    {
+        Vector3 currentPos = transform.position;
+
+        currentPos.y = Mathf.Clamp(currentPos.y, -yLimit, yLimit);
+
+        transform.position = currentPos;
+
+    }
+}
